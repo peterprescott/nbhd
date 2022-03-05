d5ebc99c797995a3f66a7761e1e706478fa296e2
Peter Prescott <prescott.peter@gmail.com>
2022-03-05 22:41:26 +0000

    Pass in db instance

--- src/nbhd/utils.py
+++ src/nbhd/utils.py
@@ -1,4 +1,8 @@
-def get_pixel(postcode):
+'''
+Assorted useful bits. Probably temporary.
+'''
+
+def get_pixel(postcode, db):
     response = db.query(f"SELECT * FROM names WHERE name1 = '{postcode.upper()}'", True)
     point = response.geometry[0]
     pixels = db.intersects('pixels', point)
